77a386f729a53d92b5fc83a807e86bcdb74aa65c
Salfff <147937926+Salfff@users.noreply.github.com>
2025-08-15 16:22:40 -0400

    update estudos-javascript folder

--- ambiente001/script2.js
+++ ambiente001/script2.js
@@ -1,6 +1,6 @@
 let vetor = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 let vetor2 = []
-var num = 20;
+var num = 2;
 if(num < 1 || num > 26) {
         console.log('Informe um número que esteja entre 1 e 26')
     } else {
