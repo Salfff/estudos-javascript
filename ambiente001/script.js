ca47919a0ce24845d179dd0a98dc7ae16b6bdebb
Salfff <147937926+Salfff@users.noreply.github.com>
2025-08-15 16:11:34 -0400

    Estudos javascript update

---
+++ ambiente001/script.js
@@ -0,0 +1,27 @@
+let input = document.getElementById('numInput')
+
+let button = document.getElementById('botao')
+
+let res = document.getElementById('res')
+
+
+button.addEventListener('click', botaoClicado)
+
+function botaoClicado() {
+    let vetor = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+    let vetorRes = []
+    let num = Number(input.value)
+
+    res.innerHTML = '';
+    input.value = ''
+    if(num < 1 || num > 26) {
+        alert('Informe um número que esteja entre 1 e 26')
+    } else {
+
+    for(let i = 0; i < num; i++) {
+        vetorRes.push(`<br> ${vetor[i]}`);
+    }
+    res.innerHTML = vetorRes
+}
+}
---
+++ ambiente001/script2.js
@@ -0,0 +1,13 @@
+let vetor = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+let vetor2 = []
+var num = 20;
+if(num < 1 || num > 26) {
+        console.log('Informe um número que esteja entre 1 e 26')
+    } else {
+
+    for(let i = 0; i < num; i++) {
+        vetor2.push(vetor[i]);
+    }
+
+    console.log(vetor2)
+}
