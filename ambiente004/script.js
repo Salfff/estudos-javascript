197949e35da262a7b03e72d280b6c7e8a9629e20
Salfff <147937926+Salfff@users.noreply.github.com>
2025-08-28 21:51:01 -0400

    ambiente004 update

---
+++ ambiente004/script.js
@@ -0,0 +1,42 @@
+const dateObject = new Date(1995, 2, 25)
+
+switch (dateObject.getMonth()) {
+  case 1: 
+    console.log('janeiro')
+    break;
+
+  case 2:
+    console.log('fevereiro')
+    break;
+
+  case 3:
+    console.log('março')
+    break;
+  case 4:
+    console.log('abril')
+    break;
+  case 5:
+    console.log('maio')
+    break;
+  case 6:
+    console.log('junho')
+    break;
+  case 7:
+    console.log('Julho')
+    break
+  case 8:
+    console.log('Agosto')
+    break;
+  case 9:
+    console.log('Setembro')
+    break;
+  case 10:
+    console.log('Outubro')
+    break;
+  case 11:
+    console.log('Novembro')
+    break;
+  case 12:
+    console.log('Dezembro')
+    break;
+}
