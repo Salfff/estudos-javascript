260a2c48874b91b194ecc08a224c0c9033c5b59b
Salfff <147937926+Salfff@users.noreply.github.com>
2025-09-07 11:51:47 -0400

    Add sample functions for average and factorial
    Created index2.js with commented-out implementations of functions to calculate the average of three numbers and the factorial of a number, including example usage.

---
+++ ambiente006/index2.js
@@ -0,0 +1,17 @@
+// function media(n1, n2, n3) {
+//   return (n1 + n2 + n3) / 3
+// }
+
+// console.log(media(2, 2, 2))
+
+
+// function fatorial(n) {
+//   let fat = 1;
+//   for(let cont = n; cont > 1; cont--) {
+//     fat *= cont;
+//   }
+//   return fat
+// }
+
+// console.log(fatorial(4))
+
