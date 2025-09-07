a4e225383db02a950274f5aa7364b60548aaa592
Salfff <147937926+Salfff@users.noreply.github.com>
2025-09-07 15:25:15 -0400

    Add users array and log user names with map
    Introduced a users array with sample data and used map to extract and log the names. This demonstrates array mapping with objects.

--- ambiente006/index3.js
+++ ambiente006/index3.js
@@ -3,3 +3,11 @@
 // const mapped = array.map((x) => x * 2)
 
 // console.log(mapped)
+
+const users = [
+  {id: 1, name: 'Jona'},
+  {id: 2, name: 'Peter'},
+  {id: 3, name: 'Andréia'}
+]
+
+console.log(users.map((user) => user.name))
