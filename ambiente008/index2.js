c1338dae3b3508eb41e9f6ec2c8670f7ccb852f1
Salfff <147937926+Salfff@users.noreply.github.com>
2025-09-09 00:55:35 -0400

    Add React HTML template and update JS example
    Added a new index.html with a basic React setup using CDN links and a simple App component. Updated index2.js to comment out the previous loop example and add a new setTimeout demonstration with console logs.

--- ambiente008/index2.js
+++ ambiente008/index2.js
@@ -1,7 +1,15 @@
-for (var i = 0; i < 3; i++) {
-  const log = () => {
-    console.log(i)
-  }
+// for (let i = 0; i < 3; i++) {
+//   const log = () => {
+//     console.log(i)
+//   }
 
-  setTimeout(log, 100)
-}
+//   setTimeout(log, 100)
+// }
+
+console.log("Início")
+
+setTimeout(() => {
+  console.log("Depois de 2 segundos")
+}, 2000)
+
+console.log("Fim")
